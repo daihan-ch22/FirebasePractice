0f79b635a020aac54128b41fe3b1d8ade9d08885
danc <daehan.dan.choi@gmail.com>
2023-08-30 22:57:08 +0900

    fcmtoken receive check

--- myserver/src/main/java/com/firebase/myserver/NotiController.java
+++ myserver/src/main/java/com/firebase/myserver/NotiController.java
@@ -1,10 +1,14 @@
 package com.firebase.myserver;
 
 
+import com.firebase.myserver.common.Constants;
+import com.google.api.Http;
 import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpResponseException;
+import org.apache.http.protocol.HTTP;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +40,21 @@ public class NotiController {
             return ResponseEntity.internalServerError().body("앗! 널값이들어왔네!");
         }
         return ResponseEntity.ok("푸쉬메시지 전송 성공");
+    }
+
+    @PostMapping("/fcm/api/token")
+    public ResponseEntity<?> getFCMToken(@RequestBody String token){
+        String response = "";
+
+        if(token != null && !token.isEmpty()) {
+            Constants.DEVICE_TOKEN = token;
+            response = "Successfully received FCM token from App.";
+            log.debug("DEVICE TOKEN == " + Constants.DEVICE_TOKEN);
+            return ResponseEntity.ok(response);
+        } else {
+            response = "Failed to receive FCM token from App.";
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
--- myserver/src/main/java/com/firebase/myserver/common/Constants.java
+++ myserver/src/main/java/com/firebase/myserver/common/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final String[] SCOPES = { MESSAGING_SCOPE };
     public static final String FCM_JSON_PATH = "fir-practice-7ae2d-firebase-adminsdk-ogel7-7b3f9214b8.json";
 
-    public static final String DEVICE_TOKEN = "ddZRBoROSZOK9ZbU3xMmwq:APA91bGsbupPoUg0KkO_QHgMp3yevmQU1qJ8PkDlOrRZai470KY4ohJd5cZHNAk5h_kbV3Mg9N_y_D7-HHw-_tTznR-ilmBe_t5u-ZR2hFP14Sq-9wSMxs3w-8lPN8CY5y-g1JG6bllY";
+    public static String DEVICE_TOKEN = "ddZRBoROSZOK9ZbU3xMmwq:APA91bGsbupPoUg0KkO_QHgMp3yevmQU1qJ8PkDlOrRZai470KY4ohJd5cZHNAk5h_kbV3Mg9N_y_D7-HHw-_tTznR-ilmBe_t5u-ZR2hFP14Sq-9wSMxs3w-8lPN8CY5y-g1JG6bllY";
 }
